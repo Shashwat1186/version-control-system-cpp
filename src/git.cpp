c123e2043550b23bbfe471b2864af13620b7e7e6
Shashwat Salil Warune <shashwatwarune@gmail.com>
2026-06-05 20:16:15 +0530

    codecrafters submit [skip ci]

--- src/git.cpp
+++ src/git.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include "zstr.hpp"
+#include <openssl/sha.h>
 
 namespace git {
 
@@ -28,16 +29,42 @@ auto init() -> void {
 }
 
 auto cat_file(std::string_view hash) -> void {
-  auto path = std::string(".git/objects/") + std::string(hash.substr(0, 2)) +
-              "/" + std::string(hash.substr(2));
+  auto path = std::string(".git/objects/") + std::string(hash.substr(0, 2)) + "/" + std::string(hash.substr(2));
 
   zstr::ifstream file(path);
-  std::string contents((std::istreambuf_iterator<char>(file)),
-                       std::istreambuf_iterator<char>());
+  if (!file) {
+    std::cerr << "Failed to open: " << path << '\n';
+    return;
+  }
+  std::string contents((std::istreambuf_iterator<char>(file)),std::istreambuf_iterator<char>());
 
   auto header_size = contents.find('\0') + 1;
 
   std::cout << std::string_view(contents.begin() + header_size, contents.end());
+}
+
+auto hash_object(std::string filename){
+  std::ifstream file(filename, std::ios::binary);
+  if (!file) {
+    std::cerr << "Failed to open: " << path << '\n';
+    return;
+  }
+  SHA_CTX shaContext;
+  SHA1_Init(&shaContext);
+  char buffer[4096];
+  size_t bytesRead;
+  while(file.read(buffer, sizeof(buffer)) || file.gcount()>0){
+    SHA1_Update(&shaContext, reinterpret_cast<unsigned char*>(buffer), file.gcount());
+  }
+  unsigned char hash[20];
+  SHA1_Final(hash, &shaContext);
+  file.close();
+  std::stringstram ss;
+  for(int i = 0 ; i<20; i++){
+    ss<<std::hex<<std::setw(2)<<std::setfill('0')<<(int)hash[i];
+  }
+  std::string hashString = ss.str();
+  std::cout<< hashString << endl;
 }
 
 } // namespace git

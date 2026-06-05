daab9220c9c66fa2042dd726ce06335fc8c27124
Shashwat Salil Warune <shashwatwarune@gmail.com>
2026-06-05 22:16:43 +0530

    codecrafters submit [skip ci]

--- src/git.cpp
+++ src/git.cpp
@@ -82,7 +82,7 @@ auto hash_object(std::string filename) -> void {
     try {
         std::filesystem::rename(filename, newPath);
         std::cout << "File moved successfully to " << destination << '\n';
-    } catch (const fs::filesystem_error& e) {
+    } catch (const std::filesystem::filesystem_error& e) {
         std::cerr << "Error moving file: " << e.what() << '\n';
     }
 }
